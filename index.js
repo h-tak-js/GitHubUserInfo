d303aa1cf75c53f5849db08f4dc8e5540fc37723
h-tak-js <86530985+h-tak-js@users.noreply.github.com>
2021-07-10 01:02:45 +0900

    処理を分離して読み易いコードへ
    大きいコミットごめんなさい

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
+'use strict';
 document.addEventListener("DOMContentLoaded", function () {
   function fetchUserInfo(user_id) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(user_id)}`)
+    return fetch(`https://api.github.com/users/${encodeURIComponent(user_id)}`)
       .then(response => {
-        const result = document.getElementById('result');
         if (response.ok) {
-          return response.json().then(user_info => {
-            console.log(user_info);
-            const view = `
-                <h4>${user_info.name} (@${user_info.login})</h4>
-                <img src="${user_info.avatar_url}" alt="${user_info.login}" height="100">
-                <dl>
-                    <dt>Location</dt>
-                    <dd>${user_info.location}</dd>
-                    <dt>Repositories</dd>
-                    <dd>${user_info.public_repos}</dd>
-                </dl>
-                `;
-            result.innerHTML = view;
-          });
+          return response.json();
         } else {
-          console.error("エラーレスポンス", response);
-          result.innerHTML = `<p><em>取得できませんでした。</em></p>`
+          display_fetch_result(`<p><em>取得できませんでした。</em></p>`);
+          return Promoise.reject(new Error(`${response.status}:${response.statusText}`));
         }
       });
   }
-  const input_user_name = document.getElementsByName('user_name')[0];
-  input_user_name.addEventListener('change', () => {
-    fetchUserInfo(input_user_name.value);
+  const button_submit = document.getElementsByName('submit')[0];
+  button_submit.addEventListener('click', () => {
+    const input_user_name = document.getElementsByName('user_name')[0];
+    fetchUserInfo(input_user_name.value)
+      .then((user_info) => create_view(user_info))
+      .then((view) => display_result(view))
+      .catch((error) => {
+        console.log(`エラーです${error}`);
+      });
   });
+
+  function create_view(user_info) {
+    return escape_html`
+    <h4>${user_info.name} (@${user_info.login})</h4>
+    <img src="${user_info.avatar_url}" alt="${user_info.login}" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>${user_info.location}</dd>
+      <dt>Repositories</dd>
+      <dd>${user_info.public_repos}</dd>
+    </dl>`;
+  }
+
+  function display_result(view) {
+    const result = document.getElementById('result');
+    result.innerHTML = view;
+  }
 });
+
+function escape_special_cahrs(str) {
+  return str
+    .replace(/&/g, '&amp')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039');
+}
+
+function escape_html(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === 'string') {
+      // 値が文字列型であればエスケープ
+      return result + escape_special_cahrs(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
+}
